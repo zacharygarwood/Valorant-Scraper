5a33e674dfbfa9800d87495e019e8cef72b03027
Zachary Garwood <garwoodzachary@gmail.com>
2021-01-12 15:59:35 -0600

    created csv file

--- src/Valorant_Scraper.java
+++ src/Valorant_Scraper.java
@@ -1,16 +1,17 @@
 import org.openqa.selenium.*;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 
 public class Valorant_Scraper {
     private static String base_url = "https://tracker.gg/valorant/profile/riot/Gwoodz%23NA1/overview?playlist=competitive";
     private static String working_url = base_url;
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         /*
         x make it quicker
         make it so there is a database // clear playerdata //
@@ -29,18 +30,15 @@ public class Valorant_Scraper {
 
         final List<String> dateGamesPlayed = createDatesGamesPlayed(allGames);
         final List<String> linksToMatches = createLinksToGames(allGames);
-        List<List<String>> playerData = new ArrayList<>(); // first list is links // second list is names // third list is ranks
+        List<List<String>> playerData = createPlayerData(linksToMatches, driver); // first list is links // second list is names // third list is ranks
 
-
-        playerData = createPlayerData(linksToMatches, driver);
-
-        for (String pr : playerData.get(2)) {
-            System.out.println(pr);
-        }
+        createPlayerDataCSV(playerData);
+        removeDuplicatesFromCSV("C:/Users/zgarw/Documents/Projects/valorant_stats_2/data/player_data.csv");
         driver.quit();
     }
 
     private static List<String> createDatesGamesPlayed(WebElement allGames) {
+        System.out.println("Getting dates to matches...");
         final List<WebElement> dateGamesPlayedWE = allGames.findElements(By.tagName("h3"));
         final List<String> dateGamesPlayed = new ArrayList<>();
         for (WebElement we : dateGamesPlayedWE) {
@@ -50,6 +48,7 @@ public class Valorant_Scraper {
     }
 
     private static List<String> createLinksToGames(WebElement allGames) {
+        System.out.println("Getting links to matches...");
         final List<WebElement> webElementsForLinksToMatches = allGames.findElements(By.tagName("a"));
         final List<String> linksToMatches = new ArrayList<>();
         for (WebElement we : webElementsForLinksToMatches) {
@@ -59,6 +58,7 @@ public class Valorant_Scraper {
     }
 
     private static List<String> createListPlayerNames(WebElement allPlayers) {
+        System.out.println("Getting player names...");
         List<WebElement> playerNamesWE = allPlayers.findElements(By.className("trn-ign"));
         List<String> playerNames = new ArrayList<>();
         for (WebElement we : playerNamesWE) {
@@ -68,6 +68,7 @@ public class Valorant_Scraper {
     }
 
     private static List<String> createLinksToPlayers(WebElement allPlayers, ChromeDriver driver) {
+        System.out.println("Getting links to players...");
         List<WebElement> playerNamesWE = allPlayers.findElements(By.className("trn-ign"));
         List<WebElement> linksToPlayersWE = new ArrayList<>();
         List<String> linksToPlayers = new ArrayList<>();
@@ -84,6 +85,7 @@ public class Valorant_Scraper {
     }
 
     private static List<String> createPlayerRanks(List<List<String>> playerData, ChromeDriver driver) throws InterruptedException {
+        System.out.println("Getting player ranks...");
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         final List<String> playerRanks = new ArrayList<>();
         for(int i = 0; i < playerData.get(0).size(); i++) {
@@ -101,6 +103,7 @@ public class Valorant_Scraper {
     }
 
     private static List<List<String>> createPlayerData(List<String> linksToMatches, ChromeDriver driver) throws InterruptedException {
+        System.out.printf("Creating player data...");
         List<List<String>> playerData = new ArrayList<>();
         for(int i = 0; i < linksToMatches.size(); i++) {
             working_url = linksToMatches.get(i);
@@ -124,7 +127,24 @@ public class Valorant_Scraper {
         return playerData;
     }
 
+    private static void createPlayerDataCSV(List<List<String>> playerData) {
+        System.out.println("Creating player data CSV file...");
+        try {
+            FileWriter fw = new FileWriter("C:/Users/zgarw/Documents/Projects/valorant_stats_2/data/player_data.csv", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            for(int i = 0; i < playerData.get(0).size(); i++) {
+                pw.println(playerData.get(1).get(i) + "," + playerData.get(2).get(i) + "," + playerData.get(0).get(i));
+                pw.flush();
+            }
+            pw.close();
+        } catch (Exception e) {
+
+        }
+    }
+
     private static List<String> removeDuplicates(List<String> list) {
+        System.out.println("Removing duplicate players...");
         List<String> newList = new ArrayList<>();
         for (String element : list) {
             if (!newList.contains(element)) {
@@ -134,6 +154,26 @@ public class Valorant_Scraper {
         return newList;
     }
 
+    private static void removeDuplicatesFromCSV(String filepath) throws Exception {
+        System.out.printf("Removing duplicates from CSV file...");
+
+        String filePath = "C:/Users/zgarw/Documents/Projects/valorant_stats_2/data/player_data.csv";
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        Set<String> lines = new HashSet<String>(10000); // maybe should be bigger
+        String line;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        reader.close();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+        for (String unique : lines) {
+            writer.write(unique);
+            writer.newLine();
+        }
+        writer.close();
+    }
 }
+
+
 
 
